34fde1e11e0eed33441af94c53dba98c506c0301
David Munoz Barco <david.f.do@hotmail.com>
2023-07-11 17:24:31 -0500

    solving assignment 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,19 +6,19 @@ import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 
 function Expenses({expenses}){
     const [filterYear, setFilterYear] = useState('2020');
+
     const filterChamgedHandler = (yearSelected) => {
         setFilterYear(yearSelected);
     };
+    
+    const filteredExpenses = expenses.filter(expense => (expense.date.getFullYear().toString()) === filterYear);
 
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter year={filterYear} onFilter={filterChamgedHandler} />
-
-                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-                <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+                {/* {expenses.map((expense, index) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)} */}
+                {filteredExpenses.map((expense, index) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
             </Card>
         </div>
     );
